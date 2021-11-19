913d1dc52ed8a0da83acaaafbaf0c02c65f0866d
Xinying Hu <xinying_hu@berkeley.edu>
2021-11-19 00:02:18 -0800

    add a thread to control servo

--- computer vision/pose_control.py
+++ computer vision/pose_control.py
@@ -1,14 +1,51 @@
+from os import terminal_size
 import cv2
 import mediapipe as mp
 import numpy as np
 import time
 import serial
+import threading
 
-#arduino = serial.Serial(port='/dev/cu.usbmodem142301', baudrate=115200, timeout=1)
+
+data_lock = threading.Lock()
+data_holder = None
+stop_threads = False
+
+arduino = serial.Serial(port='/dev/cu.usbmodem144201', baudrate=115200, timeout=1)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+
+def transmit_data_func():
+  while True:
+    if stop_threads:
+      print ('* Received keyboard interrupt, quitting threads.')
+      break
+    _timer_start = time.time()
+    data_lock.acquire()
+    if data_holder is not None:
+      data = str(data_holder).encode()
+      data_lock.release()
+    else:
+      data_lock.release()
+      continue
+    _timer_acquire = time.time()
+    arduino.write(data)    
+    _timer_write = time.time()
+    # status = arduino.readline().decode('ascii')
+    time.sleep(1)
+    _timer_read = time.time()
+    print ("timing: acc (%.5f) write (%.5f) read (%.5f)" % (
+      (_timer_acquire - _timer_start),
+      (_timer_write - _timer_acquire),
+      (_timer_read - _timer_write),
+    ))
+
+transmit_thread = threading.Thread(
+  target=transmit_data_func, name= 'transmit_thread'
+)
+transmit_thread.start()
 
 
 def calculate_angle(A_x, A_y, B_x, B_y, C_x, C_y):
@@ -25,85 +62,85 @@ def calculate_angle(A_x, A_y, B_x, B_y, C_x, C_y):
 
     return angle
 
+# return_item = "success"
+# t1 = threading.Thread(target=transmit_data, name= 't1')
 angle_list = []
-angle_output = 0
-i = 0
+angle_output = None
 cap = cv2.VideoCapture(0)
-with mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as pose:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      break
 
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.flip(image, 1)
-    image_width, image_height = image.shape[1], image.shape[0]
-    results = pose.process(image)
+try:
+  with mp_pose.Pose(
+      min_detection_confidence=0.5,
+      min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+      success, image = cap.read()
+      if not success:
+        print("Ignoring empty camera frame.")
+        break
 
-    # Draw the pose annotation on the image.
-    # image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    # mp_drawing.draw_landmarks(
-    #     image,
-    #     results.pose_landmarks,
-    #     mp_pose.POSE_CONNECTIONS,
-    #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-    
-    # calculate angles of the right arm
-    landmark_list = []
-    if results.pose_landmarks:
-      for id, landmark in enumerate(results.pose_landmarks.landmark):
-          x, y = int(landmark.x * image_width), int(landmark.y * image_height)
-          vis = landmark.visibility
-          landmark_list.append([id, x, y, vis])
-      Rshoulder_x = landmark_list[12][1]
-      Rshoulder_y = landmark_list[12][2]
-      Rshoulder_vis = landmark_list[12][3]
-      Relbow_x = landmark_list[14][1]
-      Relbow_y = landmark_list[14][2]
-      Relbow_vis = landmark_list[14][3]
-      Rwrist_x = landmark_list[16][1]
-      Rwrist_y = landmark_list[16][2]
-      Rwrist_vis = landmark_list[16][3]
+      image.flags.writeable = False
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+      image = cv2.flip(image, 1)
+      image_width, image_height = image.shape[1], image.shape[0]
+      results = pose.process(image)
 
-      cv2.circle(image, (Rshoulder_x, Rshoulder_y), 10, (0, 0, 255), cv2.FILLED)
-      cv2.circle(image, (Relbow_x, Relbow_y), 10, (0, 0, 255), cv2.FILLED)
-      cv2.circle(image, (Rwrist_x, Rwrist_y), 10, (0, 0, 255), cv2.FILLED)
-
-      if (Rshoulder_vis > 0.80 and Relbow_vis > 0.80 and Rwrist_vis > 0.80):
-        angle = calculate_angle(Rshoulder_x, Rshoulder_y, Relbow_x, Relbow_y, Rwrist_x, Rwrist_y)
-        angle_list.append(angle)
-        print("trustable data")
+      # Draw the pose annotation on the image.
+      # image.flags.writeable = True
+      image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+      # mp_drawing.draw_landmarks(
+      #     image,
+      #     results.pose_landmarks,
+      #     mp_pose.POSE_CONNECTIONS,
+      #     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
       
-      print("Rshoulder:", Rshoulder_x, Rshoulder_y)
-      print("Relbow:", Relbow_x, Relbow_y)
-      print("Rwrist:", Rwrist_x, Rwrist_y)
-      print("visibility:", Rshoulder_vis, Relbow_vis, Rwrist_vis)
-      
+      # calculate angles of the right arm
+      landmark_list = []
+      if results.pose_landmarks:
+        for id, landmark in enumerate(results.pose_landmarks.landmark):
+            x, y = int(landmark.x * image_width), int(landmark.y * image_height)
+            vis = landmark.visibility
+            landmark_list.append([id, x, y, vis])
+        Rshoulder_x = landmark_list[12][1]
+        Rshoulder_y = landmark_list[12][2]
+        Rshoulder_vis = landmark_list[12][3]
+        Relbow_x = landmark_list[14][1]
+        Relbow_y = landmark_list[14][2]
+        Relbow_vis = landmark_list[14][3]
+        Rwrist_x = landmark_list[16][1]
+        Rwrist_y = landmark_list[16][2]
+        Rwrist_vis = landmark_list[16][3]
 
-      if(len(angle_list) == 5):
-          angle_output = np.average(angle_list)
-          angle_list = []
+        cv2.circle(image, (Rshoulder_x, Rshoulder_y), 10, (0, 0, 255), cv2.FILLED)
+        cv2.circle(image, (Relbow_x, Relbow_y), 10, (0, 0, 255), cv2.FILLED)
+        cv2.circle(image, (Rwrist_x, Rwrist_y), 10, (0, 0, 255), cv2.FILLED)
 
-      print("angle:", angle_output)
+        if (Rshoulder_vis > 0.80 and Relbow_vis > 0.80 and Rwrist_vis > 0.80):
+          angle = calculate_angle(Rshoulder_x, Rshoulder_y, Relbow_x, Relbow_y, Rwrist_x, Rwrist_y)
+          angle_list.append(angle)
+          print("trustable data")
+          print("Rshoulder:", Rshoulder_x, Rshoulder_y)
+          print("Relbow:", Relbow_x, Relbow_y)
+          print("Rwrist:", Rwrist_x, Rwrist_y)
+          print("visibility:", Rshoulder_vis, Relbow_vis, Rwrist_vis)
+        
 
-      # if i == 20:
-      #   arduino.write(str(angle_output).encode())
-      #   print(arduino.readline().decode('ascii'))
-      #   i = 0
-      i += 1 
-      
-      
+        if(len(angle_list) == 5):
+            angle_output = np.average(angle_list)
+            angle_list = []
+            data_lock.acquire()
+            data_holder = angle_output
+            data_lock.release()
 
-    # Flip the image horizontally for a selfie-view display.
-      cv2.putText(image, "angle:{}".format(angle_output), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
-      (255, 0, 255), 3)
-    cv2.imshow('MediaPipe Pose', image)
+      # Flip the image horizontally for a selfie-view display.
+        cv2.putText(image, "angle:{}".format(angle_output), (10, 120), cv2.FONT_HERSHEY_PLAIN, 3,
+        (255, 0, 255), 3)
+      cv2.imshow('MediaPipe Pose', image)
 
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+      if cv2.waitKey(5) & 0xFF == 27:
+        break
+
+except (KeyboardInterrupt, SystemExit):
+  print ('\n* Received keyboard interrupt, quitting main.')
+  stop_threads = True
+  cap.release()
+  transmit_thread.join()
