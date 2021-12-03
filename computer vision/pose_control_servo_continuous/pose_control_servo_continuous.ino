17146d7dc7c8ce0e1a619f29b7763571d7d0f0f4
Xinying Hu <xinying_hu@berkeley.edu>
2021-12-02 22:09:17 -0800

    organize + interpolate arduino code

--- computer vision/pose_control_servo_continuous/pose_control_servo_continuous.ino
+++ computer vision/pose_control_servo_continuous/pose_control_servo_continuous.ino
@@ -1,7 +1,6 @@
 #include <Servo.h>
 String InBytes;
 int BytesToInt;
-int CurrentServo = 0;
 int x = 0;
 int toggle = 0;
 Servo myservo;
@@ -12,37 +11,16 @@ void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
   pinMode(LED_BUILTIN, OUTPUT);
-  //Serial.print("booting");
- 
-
 }
 
 void loop() {
-//   InBytes = Serial.readStringUntil('\n');
-//    BytesToInt = InBytes.toInt();
-//    Serial.write("Success!\n");
-  
-  
+
   if (Serial.available() > 0){
     x++;
     InBytes = Serial.readStringUntil('\n');
     BytesToInt = InBytes.toInt();
     myservo.write(BytesToInt);
     
-//    if bytestoint < current servo
-//      current servo ++
-
-//    while (BytesToInt != CurrentServo){
-//      if (BytesToInt < CurrentServo){
-//        CurrentServo ++;
-//        myservo.write(CurrentServo);
-//      }
-//      if (BytesToInt > CurrentServo){
-//        CurrentServo --;
-//        myservo.write(CurrentServo);
-//      }
-//    }
-//    Serial.write("Success!\n");
     
     if (x > 5){
       digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
